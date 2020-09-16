58856cbe3eaf38e5254921c6e303cfe506e8190a
JoeyStrandnes <joey.strandnes@hotmail.com>
2020-09-16 21:41:25 +0200

    Added example for ESP8266
    This example useses a button to start a timer and light a LED while the timer is active.
    This example assumes that the timer is active and NOT stopped through other means.
    ***Will make an "advanced" button example that checks if the timer is still active. Kind of overkill though...

---
+++ examples/Button_Timer/Button_Timer.ino
@@ -0,0 +1,73 @@
+/*
+Uses a momentary button to start a time entry and light a LED. 
+The Timer will stop when teh button is pressed a seccond time.
+
+Only for ESP8266
+*/
+
+#include <Toggl.h>
+
+const char* SSID = "SSID";
+const char* PASS = "PASSWORD";
+
+
+String TimerID{};
+int const PID{123456789};                   //Project ID is specific to each user project
+String const Token{"TOKEN"};                //API Token is found in "Profile Settings" 
+
+const int buttonPin = D7;
+const int ledPin =  D8;
+
+int buttonState = 0;
+bool TOGGLE = true;
+bool BUTTON = false;
+
+
+void ICACHE_RAM_ATTR handleInterrupt ();
+
+
+Toggl toggl;
+
+
+void setup(){
+
+    Serial.begin(115200);
+
+    pinMode(ledPin, OUTPUT);
+    pinMode(buttonPin, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(buttonPin), handleInterrupt, FALLING);
+    
+    toggl.init(SSID,PASS);
+    toggl.setAuth(Token);
+    
+}
+
+
+void handleInterrupt() {
+  BUTTON = true;
+}
+
+
+
+void loop(){
+
+  if(TOGGLE == true && BUTTON == true){
+    digitalWrite(ledPin, HIGH);
+    TimerID = (toggl.StartTimeEntry("HelloWorld", "TagName", PID, "ESP-Button"));
+    TOGGLE = false;
+    BUTTON = false;
+    delay(1000);   
+  }
+
+
+  if (TOGGLE == false && BUTTON == true){
+    digitalWrite(ledPin, LOW);
+    toggl.StopTimeEntry(TimerID);
+    TOGGLE = true;
+    BUTTON = false;
+    delay(1000);
+  }
+
+  
+}
