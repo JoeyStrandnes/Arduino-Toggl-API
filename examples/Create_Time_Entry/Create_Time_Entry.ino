dae9cba535028d4a200309043812d70b301d120d
JoeyStrandnes <joey.strandnes@hotmail.com>
2020-10-20 19:15:22 +0200

    Cleaned up the example

--- examples/Create_Time_Entry/Create_Time_Entry.ino
+++ examples/Create_Time_Entry/Create_Time_Entry.ino
@@ -5,18 +5,19 @@ Create a time entry that lasts 1 hour with a description "HelloWorld", use the t
 
 #include <Toggl.h>
 
-const char* SSID{"SSID"};
-const char* PASS{"PASSWORD"};
+char* const SSID{"SSID"};
+char* const PASS{"PASSWORD"};
 
 
-String const Token{"TOKEN"};               //API Token is found in "Profile Settings" 
+String const Token{"TOKEN"};                  //API Token is found in "Profile Settings" 
 
 // Timer details
-char* const Description{"HelloWorld!"};   //Description of time entry.
-char* const Tag{""};                      //Tag name, leave empty if no tag is used. 
-int   const Duration{3600};               //Duration in secconds.
-int   const PID{123456789};               //Project ID is specific to each user project.
-char* const CreatedWith{"ESP"};
+char* const Description{"HelloWorld!"};       //Description of time entry.
+char* const Tag{""};                          //Tag name, leave empty if no tag is used. 
+int   const Duration{3600};                   //Duration in secconds.
+char* const Start{"2020-09-24T12:00:00.000Z"};//Start time in ISO 8601 format.
+int   const PID{123456789};                   //Project ID is specific to each user project.
+char* const CreatedWith{"ESP"};               //Name of "client".
 
 Toggl toggl;
 
@@ -25,8 +26,7 @@ void setup(){
   toggl.init(SSID,PASS);  
   toggl.setAuth(Token);
 
-
-  CreateTimeEntry(String Description, String Tags, int Duration, String Start, int PID, String CreatedWith); // Returns the timer ID as a String
+  toggl.CreateTimeEntry(Description, Tag, Duration, Start, PID, CreatedWith);
   
 }
 
